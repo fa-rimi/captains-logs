67915e6d992b469819dddf8764f726bd94950807
rimi <a.fatimaarimi@gmail.com>
2023-09-07 16:00:00 -0400

    completed Show.jsx

--- views/Show.jsx
+++ views/Show.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+// views/Show.jsx
+const React = require("react"); // Import React
 
-const Show = () => {
+const Show = (props) => {
+  const { log } = props;
+
   return (
-    <div>Show</div>
-  )
-}
+    <div>
+      <h1>Log Details</h1>
+      <p>
+        <strong>Title:</strong> {log.title}
+      </p>
+      <p>
+        <strong>Entry:</strong> {log.entry}
+      </p>
+      <p>
+        <strong>Is Ship Broken:</strong> {log.isShipBroken ? "Yes" : "No"}
+      </p>
+      {log.createdAt && (
+        <p>
+          <strong>Created At:</strong>{" "}
+          {new Date(log.createdAt).toLocaleString()}
+        </p>
+      )}
+      <a href="/logs">Back to Log Index</a>
+    </div>
+  );
+};
 
-export default Show
+export default Show;
