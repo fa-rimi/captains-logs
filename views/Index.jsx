7479ca25f56cfb7206e57224fff373675ca3ff50
rimi <a.fatimaarimi@gmail.com>
2023-09-07 14:46:59 -0400

    renders entry and boolean

--- views/Index.jsx
+++ views/Index.jsx
@@ -6,9 +6,15 @@ const Index = (props) => {
     <div>
       <h1>Log Index</h1>
       <ul>
-        {/* Render a list of log titles */}
+        {/* Render a list of log details including title, entry, and ship status */}
         {props.logs.map((log) => (
-          <li key={log._id}>{log.title}</li>
+          <li key={log._id}>
+            <strong>Title:</strong> {log.title}
+            <br />
+            <strong>Entry:</strong> {log.entry}
+            <br />
+            <strong>Is Ship Broken:</strong> {log.isShipBroken ? "Yes" : "No"}
+          </li>
         ))}
       </ul>
       <a href="/logs/new">Create Log</a>
